3ac2f39984fd77b312eda043c51e4d97e9573a90
lashi00L <131507983+lashi00L@users.noreply.github.com>
2023-08-22 11:30:12 +0530

    creating main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-. 
+from fastapi import FastAPI
+from reactpy.backend.fastapi import configure
+from reactpy import component,event,html,use_state
+import reactpy as rp
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import mongo_client
+
